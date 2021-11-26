dab256fe30d579f45cb092add606b9bc0722a66d
johnsonjoseph37 <84423466+johnsonjoseph37@users.noreply.github.com>
2021-11-25 20:15:49 -0500

    After running three experiments

--- create_splits.py
+++ create_splits.py
@@ -7,27 +7,43 @@ import numpy as np
 
 from utils import get_module_logger
 
+import shutil
 
-def split(source, destination):
+def split(data_dir):
     """
-    Create three splits from the processed records. The files should be moved to new folders in the
+    Create three splits from the processed records. The files should be moved to new folders in the 
     same directory. This folder should be named train, val and test.
 
     args:
-        - source [str]: source data directory, contains the processed tf records
-        - destination [str]: destination data directory, contains 3 sub folders: train / val / test
+        - data_dir [str]: data directory, /home/workspace/data/waymo
     """
-    # TODO: Implement function
+    
+    # TODO: Split the data present in `/home/workspace/data/waymo/training_and_validation` into train and val sets.
+    # You should move the files rather than copy because of space limitations in the workspace.
+    
+    src_file_dir = '/home/workspace/data/waymo/processed/'
+	
+    file_names = os.listdir(src_file_dir)
+    num_files = len(file_names)
+    
+    random.shuffle(file_names)
+	
+    for i in range(num_files):
+        src_path = src_file_dir + file_names[i]
 
+        if i<.8*num_files:
+            dst_path = data_dir + 'train/' + file_names[i]
 
-if __name__ == "__main__":
+        else:
+            dst_path = data_dir + 'val/' + file_names[i]
+
+        shutil.copy(src_path, dst_path)   	
+        
+if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
-    parser.add_argument('--source', required=True,
-                        help='source data directory')
-    parser.add_argument('--destination', required=True,
-                        help='destination data directory')
+    parser.add_argument('--data_dir', required=True, help='data directory')
     args = parser.parse_args()
 
     logger = get_module_logger(__name__)
     logger.info('Creating splits...')
-    split(args.source, args.destination)
+    split(args.data_dir)
